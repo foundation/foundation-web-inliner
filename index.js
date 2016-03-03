067ca9c97c671bfcd3648badb788d53a4d13626f
Geoff Kimball <geoff@zurb.com>
2016-03-03 12:29:04 -0800

    Remove placeholder text and only run inliner if the HTML field has a value

--- index.js
+++ index.js
@@ -6,44 +6,16 @@ var inlineCss = require('inline-css');
 var parser = new DOMParser();
 var siphonMQ = require('siphon-media-query');
 
-var PH_HTML =
-`<html>
-  <head>
-    <style>
-      .button { color: blue; }
-
-      @media screen and (min-width: 600px) {
-        .button { background-color: red; }
-      }
-    </style>
-  </head>
-  <body>
-    <div class="button"></div>
-  </body>
-</html>`;
-
-var PH_CSS =
-`.button {
-  font-size: 2rem;
-}
-
-@media (min-width: 200px) {
-  .button {
-    font-size: 10rem;
-  }
-}`;
-
 var htmlInput = document.querySelector('[data-input-html]');
 var cssInput = document.querySelector('[data-input-css]');
 var output = document.querySelector('[data-output]');
 var compileButton = document.querySelector('[data-compile]');
 
-htmlInput.value = PH_HTML;
-cssInput.value = PH_CSS;
-
 compileButton.addEventListener('click', inline);
 
 function inline() {
+  if (!htmlInput.value) return;
+
   var css = cssInput.value;
 
   var inlineOpts = {
