7d070598fdef673c876f70b1977297cf65ea5e66
Geoff Kimball <geoff@zurb.com>
2016-03-03 14:42:08 -0800

    Add option for compression and move the <style> tag into the <body>

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ var escapeHtml = require('escape-html');
 var inlineCss = require('inline-css');
 var parser = new DOMParser();
 var siphonMQ = require('siphon-media-query');
+var htmlMinifier = require('html-minifier').minify;
 
 var htmlInput = document.querySelector('[data-input-html]');
 var cssInput = document.querySelector('[data-input-css]');
 var output = document.querySelector('[data-output]');
 var compileButton = document.querySelector('[data-compile]');
+var compressFlag = document.querySelector('[data-compress]');
+
+var DOCTYPE = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+
+var HTML_TAG = '<html xmlns="http://www.w3.org/1999/xhtml">';
 
 compileButton.addEventListener('click', inline);
 
@@ -19,6 +25,7 @@ function inline() {
   var html = htmlInput.value;
   var css = cssInput.value;
   var mqCss = siphonMQ(css);
+  var compressEnabled = compressFlag.checked;
 
   var inlineOpts = {
     url: '/',
@@ -26,6 +33,13 @@ function inline() {
     preserveMediaQueries: true,
     applyStyleTags: !!hasStyleTag(html),
     applyLinkTags: false
+  }
+
+  var compressOpts = {
+    collapseWhitespace: true,
+    removeComments: true,
+    keepClosingSlash: true,
+    minifyCSS: true
   }
 
   var beautifyOpts = {
@@ -37,6 +51,7 @@ function inline() {
     .then(function(html) {
       // Convert the raw string into a full DOM tree
       var dom = parser.parseFromString(html, 'text/html');
+      var domBody = dom.querySelector('body');
 
       if (!dom.querySelector('style')) {
         var styleTag = document.createElement('style');
@@ -46,10 +61,26 @@ function inline() {
       // Append media query-specific CSS to the <style> tag
       styleTag.innerHTML += mqCss;
 
+      // Move the style tag into the body
+      domBody.insertBefore(styleTag, domBody.firstChild);
+
       // Convert the DOM tree back to a string
       var newHtml = $(dom).children('html').html();
 
-      output.innerHTML = escapeHtml(beautifyHtml(newHtml, beautifyOpts));
+      // We lose the doctype and <html> tags in this whole process, so add them back
+      newHtml = DOCTYPE + HTML_TAG + newHtml + '</html>';
+
+      // If compression is enabled, compress the HTML
+      // Otherwise, fix the indentation with beautify
+      if (compressEnabled) {
+        newHtml = htmlMinifier(newHtml, compressOpts);
+      }
+      else {
+        newHtml = beautifyHtml(newHtml, beautifyOpts);
+      }
+
+      // Finally, escape the HTML so the output in the browser is literal
+      output.innerHTML = escapeHtml(newHtml);
     });
 }
 
